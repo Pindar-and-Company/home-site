a6a832a30f5e0bc2ce92a5da4eea24f9847792ac
Valentine Okundaye <107582338+Valtaire-O@users.noreply.github.com>
2025-09-28 16:54:05 -0400

    mobile view edits

--- src/components/ContactUsCard/ContactUsCard.jsx
+++ src/components/ContactUsCard/ContactUsCard.jsx
@@ -8,6 +8,9 @@ import cityImage from '../../assets/contact_us.jpg';
 function ContactSection() {
   return (
     <section className="contact-section">
+      <div className="mobile-contact-image">
+        <img src={cityImage} alt="City buildings" />
+      </div>
       <div className="contact-content">
         <h2>How can we assist you?</h2>
         <button className="contact-button">
