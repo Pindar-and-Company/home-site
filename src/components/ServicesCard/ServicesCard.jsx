cbcafd309655facb5ee081141a532e44b2d3f375
Valentine Okundaye <107582338+Valtaire-O@users.noreply.github.com>
2025-10-02 19:20:46 -0400

    misc misc

--- src/components/ServicesCard/ServicesCard.jsx
+++ src/components/ServicesCard/ServicesCard.jsx
@@ -4,8 +4,8 @@ import './ServicesCard.css';
 import { useNavigate } from 'react-router-dom';
 
 // Import your city/building image
-import backgroundimg from '../../assets/services.jpg';
-
+import backgroundimg from '../../assets/services.png';
+//const  backgroundimg = 'https://images.unsplash.com/photo-1561449501-e9d524a9fa09?q=80&w=872&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
 function ServicesSection() {
     const [activeService, setActiveService] = useState('system-design');
